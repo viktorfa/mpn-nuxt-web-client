11964fc88c60e2733b174760d129f1df54d2983d
viktorfa <vikfand@gmail.com>
2019-09-10 15:34:56 +0200

    Fix tests. Update some dependencies.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^~/(.*)$': '<rootDir>/src/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    '^vue$': 'vue/dist/vue.common.js',
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/file-mock.js",
+      "\\.(css|less)$": "<rootDir>/mocks/file-mock.js"
   },
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
